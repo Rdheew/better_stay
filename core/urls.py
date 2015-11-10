48e0796ca7ffad4fb9b635e09ac1943005277fc1
Rdheew <murtada.alali@gmail.com>
2015-11-10 21:47:14 +0000

    Fix

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
     url(r'^user/', include('registration.backends.simple.urls')),
     url(r'^user/', include('django.contrib.auth.urls')),
+    url(r'^hotel/create/$', HotelCreateView.as_view(), name='hotel_create'),
 )
--- core/views.py
+++ core/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
 
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
+
+class HotelCreateView(CreateView):
+    model = Hotel
+    template_name = "hotel/hotel_form.html"
+    fields = ['title', 'description']
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(HotelCreateView, self).form_valid(form)
