24c53ce7b8e448cfe462ae34f58e5543ddef4e18
Rdheew <murtada.alali@gmail.com>
2015-11-11 18:19:18 +0000

    Add required user authentication

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls import patterns, include, url
 from .views import *
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
     url(r'^user/', include('registration.backends.simple.urls')),
     url(r'^user/', include('django.contrib.auth.urls')),
-    url(r'^hotel/create/$', HotelCreateView.as_view(), name='hotel_create'),
+    url(r'^hotel/create/$', login_required(HotelCreateView.as_view()), name='hotel_create'),
+    url(r'hotel/$', login_required(HotelListView.as_view()), name='hotel_list'),
     url(r'hotel/$', HotelListView.as_view(), name='hotel_list'),
-    url(r'hotel/$', HotelListView.as_view(), name='hotel_list'),
-    url(r'^hotel/(?P<pk>\d+)/$', HotelDetailView.as_view(), name='hotel_detail'),
-    url(r'^hotel/update/(?P<pk>\d+)/$', HotelUpdateView.as_view(), name='hotel_update'),
-    url(r'^hotel/delete/(?P<pk>\d+)/$', HotelDeleteView.as_view(), name='hotel_delete'),
-    url(r'^hotel/(?P<pk>\d+)/review/create/$', ReviewCreateView.as_view(), name='review_create'),
-    url(r'^hotel/(?P<hotel_pk>\d+)/review/update/(?P<review_pk>\d+)/$', ReviewUpdateView.as_view(), name='review_update'),
-    url(r'^hotel/(?P<hotel_pk>\d+)/review/delete/(?P<review_pk>\d+)/$', ReviewDeleteView.as_view(), name='review_delete'),
+    url(r'^hotel/(?P<pk>\d+)/$', login_required(HotelDetailView.as_view()), name='hotel_detail'),
+    url(r'^hotel/update/(?P<pk>\d+)/$', login_required(HotelUpdateView.as_view()), name='hotel_update'),
+    url(r'^hotel/delete/(?P<pk>\d+)/$', login_required(HotelDeleteView.as_view()), name='hotel_delete'),
+    url(r'^hotel/(?P<pk>\d+)/review/create/$', login_required(ReviewCreateView.as_view()), name='review_create'),
+    url(r'^hotel/(?P<hotel_pk>\d+)/review/update/(?P<review_pk>\d+)/$', login_required(ReviewUpdateView.as_view()), name='review_update'),
+    url(r'^hotel/(?P<hotel_pk>\d+)/review/delete/(?P<review_pk>\d+)/$', login_required(ReviewDeleteView.as_view()), name='review_delete'),
 )
